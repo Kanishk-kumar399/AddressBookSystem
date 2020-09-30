c73d5035a19234582e61dd4f78b158df1462c81d
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-30 20:10:54 +0530

    Added UC3 for editing the contact details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,24 +1,57 @@
-import java.util.ArrayList;
+import java.util.*;
 public class AddressBookMain
 {
-	int numOfContact=0;
-	private Contact[] contactArray;
-	
+	Scanner sc=new Scanner(System.in);
+	private ArrayList<Contact> contactArray;
+	private Map<String,Contact> contactMap;
 	public AddressBookMain()
 	{
-		contactArray=new Contact[5];
+		contactArray=new ArrayList<>();
+		contactMap=new HashMap<>();
 	}
-	private void addNewContact(String firstName,String lastName,String address,String city,String state,int pinCode,int phoneNumber,String emailId)
+	public void addNewContact(String firstName,String lastName,String address,String city,String state,int pinCode,long phoneNumber,String emailId)
 	{
-		contactArray[numOfContact]=new Contact(firstName,lastName,address,city,state,pinCode,phoneNumber,emailId);
-		System.out.println(contactArray[numOfContact]);
-		numOfContact++;
+		Contact obj=new Contact(firstName,lastName,address,city,state,pinCode,phoneNumber,emailId);
+		contactArray.add(obj);
+		contactMap.put(firstName,obj);
 	}
-
+	public void printContact()
+	{
+	System.out.println(contactArray);
+	}
+	public void editContact()
+	{
+		System.out.println("Enter The First Name to edit the contact details");
+		String fn=sc.nextLine();
+		Contact obj=contactMap.get(fn);
+		System.out.println();
+		System.out.println("Enter the Address");
+		String ad=sc.next();
+		obj.setAddress(ad);
+		System.out.println("Enter the City");
+		String ci=sc.next();
+		obj.setCity(ci);
+		System.out.println("Enter the State");
+		String st=sc.next();
+		obj.setState(st);
+		System.out.println("Enter the Pincode");
+		int pin=sc.nextInt();
+		obj.setPinCode(pin);
+		System.out.println("Enter the Number");
+		long num=sc.nextLong();
+		obj.setPhoneNumber(num);
+		System.out.println("Enter the Email");
+		String em=sc.next();
+		obj.setEmailId(em);
+	}
 	public static void main(String args[])
 	{
 		AddressBookMain Contact1=new AddressBookMain();
-		Contact1.addNewContact("Kumar","Kanishk","SH-3/40","Varanasi","UP",2210,884024123,"kanishk@gmail.com");
+		Contact1.addNewContact("yusm","dsdsan","dsdsH","agri","UP",5545,5478686,"shivssds@gmail.com");
 		Contact1.addNewContact("Ram","Sharan","SHHH","Delhi","MP",3345,7688686,"shivam@gmail.com");
+		Contact1.printContact();
+		Contact1.editContact();
+		System.out.println("After Editing:");
+		Contact1.printContact();
 	}
 }
